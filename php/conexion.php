54dd1687cd7baa9f149bf12d7242bb4732954623
Gixarde3 <antoniochavezmarco@gmail.com>
2021-05-25 23:25:55 -0500

    Mejoras en la organización de carpetas

--- php/conexion.php
+++ php/conexion.php
@@ -1,7 +1,7 @@
 <?php
 function conectar(){
 define('DB_SERVER','localhost');
-define('DB_NAME','live-up');
+define('DB_NAME','id15290561_liveup');
 define('DB_USER','root');
 define('DB_PASS','');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
